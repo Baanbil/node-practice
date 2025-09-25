42033ca24b20b5cbb4c40ba31c31ab31423fcd4b
Baanbil Godfred <athellobills@gmail.com>
2025-09-25 09:28:40 +0000

    Updated the previous code

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-const math = require("./math")
-
-console.log("3 + 3 = ", math.add(3, 3))
-console.log("4 * 3 = ", math.multiply(4, 3))
+require("./calculator")
--- calculator.js
+++ calculator.js
@@ -1,30 +1,5 @@
-const readline = require("readline")
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-rl.question("Enter yoyr first number ", (num1) => {
-  rl.question("Enter your second number ", (num2) => {
-    rl.question("Enter your operation ", (op) => {
-      num1 = parseFloat(num1)
-      num2 = parseFloat(num2)
-      let result;
-      switch(op) {
-        case "+":
-          result =  num1 + num2;
-          break;
-        case "-":
-          result = num1 - num2;
-          break;
-        case "*":
-          result = num1 * num2;
-          break;
-        case "/":
-          result = num1 / num2;
-          break;
-      }
-      console.log(result)
-      rl.close()
-    })
-  })
-})
+const math = require("./math")
+
+console.log("Calculator is running")
+console.log("3 + 4 = ", math.add(3, 4))
+console.log("6 * 8 = ", math.multiply(6, 8))
